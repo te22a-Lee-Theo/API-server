ea9f37ff4e6e2f74baaaa7bbabf5778749b31d12
Theo Lee <theo.leehao@elev.ga.ntig.se>
2025-01-16 11:24:06 +0100

    commit 2

--- Api server/Program.cs
+++ Api server/Program.cs
@@ -1,6 +1,62 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Urls.Add("http://localhost:5046");
+app.Urls.Add("http://*:5046");
+
+List<Teacher> teachers = [
+    new () {Name = "Micke"},
+    new () {Name = "Alexander"},
+    new () {Name = "Martin"}
+];
+
+app.MapGet("/", GetMe);
+app.MapGet("/orb", GetAlso);
+app.MapGet("/micke", GetMicke);
+app.MapGet("/message", SayHello);
+app.MapGet("/teachers", GetTeachers);
+app.MapGet("/teacher/{n}", GetTeacher);
+
 
 app.Run();
+
+
+
+List<Teacher> GetTeachers()
+{
+    return teachers;
+}
+
+IResult GetTeacher(int n)
+{
+    if (n >= 0 && n < teachers.Count)
+    {
+        return Results.Ok(teachers[n]);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+}
+
+
+static void SayHello(string message)
+{
+    System.Console.WriteLine(message);
+}
+
+static Teacher GetMicke()
+{
+    Teacher micke = new() {Name = "Micke"};
+    return micke;
+}
+
+static string GetAlso()
+{
+    return "Hi, orb!";
+}
+
+static string GetMe()
+{
+    return "I know where you live";
+}
